14ca5873240206b04140e6cf7d0fc72cfd4027b1
RaulSol <icys1616@gmail.com>
2023-05-05 19:00:09 -0600

    05/05/2023

---
+++ Extra3_Layouts_Java/src/main/java/JRMS/Main.java
@@ -0,0 +1,8 @@
+package JRMS;
+
+public class  Main {
+    public static void main(String[] args) {
+        //Ventana wn = new Ventana("Layouts");
+        VentanaGrid gwn = new VentanaGrid("Layouts 2");
+    }
+}
